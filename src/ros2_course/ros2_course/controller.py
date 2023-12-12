3b2af3027915dc6dd68dc5e8d0cca89a63ec4b22
DoczyKristof <44243837+DoczyKristof@users.noreply.github.com>
2023-12-12 09:09:31 +0100

    FINITOOOOOOOO WOHOOOOOOOOOO

--- src/ros2_course/ros2_course/controller.py
+++ src/ros2_course/ros2_course/controller.py
@@ -5,7 +5,7 @@ from sensor_msgs.msg import LaserScan
 
 class MoveTurtlebotNode(Node):
     def __init__(self):
-        super().__init__('moveturtlebot')
+        super().__init__('controller')
         self.publisher = self.create_publisher(Twist, '/cmd_vel', 10)
         self.subscription = self.create_subscription(
             LaserScan,
@@ -14,25 +14,36 @@ class MoveTurtlebotNode(Node):
             10)
         self.msg = Twist()
 
+    '''
+    front_distance, front_right_distance, front_left_distance = 500.0, 500.0, 500.0    
+    
+    def checkFrontValues():
+        return front_distance < treshold or front_right_distance < treshold or front_left_distance < treshold
+    '''
+    
     def listener_callback(self, msg):
-        range_ahead = msg.ranges[-1]  #forward range
-        self.get_logger().info('range ahead: "%s"' % range_ahead)
-        if range_ahead < 0.25:
+        treshold = 0.8
+        ranges = msg.ranges
+        front_distance = ranges[0]
+        front_right_distance = ranges[5]
+        front_left_distance = ranges[-5]
+        self.get_logger().info('range ahead: "%s"' % front_distance)
+        if front_distance < treshold or front_right_distance < treshold or front_left_distance < treshold:
             self.msg.linear.x = 0.0
-            self.msg.angular.z = 0.5
+            self.msg.angular.z = 2.5
         else:
-            self.msg.linear.x = 0.25
+            self.msg.linear.x = 0.5
             self.msg.angular.z = 0.0
         self.publisher.publish(self.msg)
 
 def main(args=None):
     rclpy.init(args=args)
 
-    move_turtlebot_node = MoveTurtlebotNode()
+    controller = MoveTurtlebotNode()
 
-    rclpy.spin(move_turtlebot_node)
+    rclpy.spin(controller)
 
-    move_turtlebot_node.destroy_node()
+    controller.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
